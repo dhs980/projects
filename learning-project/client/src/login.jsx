adeff6bfc65c2c50d8fa89280d5eeadbcef421a8
dhs980 <hariharasudhanoff@gmail.com>
2025-12-31 21:54:44 +0530

    side Branch

---
+++ learning-project/client/src/login.jsx
@@ -0,0 +1,12 @@
+export function Login() {
+  return (
+    <div className="container">
+      <p className="text">
+        UserName<input type="text"></input>
+      </p>
+      <p className="text">
+        Password<input type="password"></input>
+      </p>
+    </div>
+  );
+}
